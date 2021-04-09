05bded5db97613eea43b0ec022955164cf1dec18
Beatriz Anton <beatriz.anton-artaza@ens.uvsq.fr>
2021-04-09 09:13:58 +0200

    pusgh

--- projet_robot_ricohet.py
+++ projet_robot_ricohet.py
@@ -29,7 +29,7 @@ NB_LINE = HAUTEUR // COTE
 tableau = None
 
 ##############################################
-# fonctions
+# fonctionsblabla
 
 def quadrilage():
     """Affiche un quadrilage sur le canvas."""
